1a3addb7b77d0e9533910268f06ef66daaed430f
shinyscyther <mesibovj@gmail.com>
2019-01-31 12:06:56 -0500

    set up skeleton html files for home, reserve, and tables. Tweaked code for proper paths in the first few lines of server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 //This is where I will add functionality.
 var express = require("express");
 var path = require("path");
-var connection = require("connection");
+var connection = require("./database/connection");
 var app = express();
 var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
