af29f1a3d5b4519d28a5662a2e0b0a1ace8cd8eb
shinyscyther <mesibovj@gmail.com>
2019-01-31 11:18:15 -0500

    established requirements for server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 //This is where I will add functionality.
+var express = require("express");
+var path = require("path");
+var connection = require("connection");
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
